31c20319d0b75c26a4a04c7000f3efc49a3717c4
Kranthi <kranthireddy.mekala@gmail.com>
2025-02-01 13:06:51 +0530

    Swapping Variables

--- Swapping2Numbers.java
+++ Swapping2Numbers.java
@@ -10,6 +10,31 @@ public class Swapping2Numbers {
         b = temp;
         System.out.println("After Swapping using Third Varible " +"a : "+a + " b: " + b);
 
+        //Logic -- 2 use + & - without using third variable
 
+        a = a+b; // a=30
+        b=a-b ;// 30-10 = 20
+        a = a-b; //30-20 = 10
+        System.out.println("After Swapping using use + & - " +"a : "+a + " b: " + b);
+
+        //Logic -- 3 use * and / without using third variable
+        // -- if a and b are not zero
+
+        a = a*b; //20*10 = 200
+        b = a/b; //200/20 = 10
+        a = a/b;  // 200/10 = 20
+
+        System.out.println("After Swapping using * and / " +"a : "+a + " b: " + b);
+
+        // Logic -- 4 use Bitwise XOR (^) Operator
+        a = a^b; // 20 ^ 10 = 30
+        b = a^b; // 30 ^ 10 = 20
+        a = a^b; // 30 ^ 20 = 10
+        System.out.println("After Swapping using Bitwise XOR (^) Operator " +"a : "+a + " b: " + b);
+
+        // Logic -- 5 Single Statement
+        //<-------------
+        b = a+b - (a=b);
+        System.out.println("After Swapping using Single Statement " +"a : "+a + " b: " + b);
     }
 }
