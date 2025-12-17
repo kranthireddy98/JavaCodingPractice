aa48babe156e2874e9e179d5c6c1c335182e6cf8
kranthi <kranthireddy.mekala@gmail.com>
2025-12-17 10:56:10 +0530

    Array Assignments

---
+++ ArraysAssignment/ArrayFromPermutation.java
@@ -0,0 +1,25 @@
+package ArraysAssignment;
+
+import java.util.Arrays;
+
+public class ArrayFromPermutation {
+
+    public static void main(String[] args) {
+        int[] nums = {0,2,1,5,3,4};
+
+        int[] res = buildArray(nums);
+        System.out.println(Arrays.toString(res));
+    }
+
+    static public int[] buildArray(int[] nums) {
+
+        int[] out  =new int[nums.length];
+
+        for(int i =0;i<nums.length;i++)
+        {
+            out[i] = nums[nums[i]];
+        }
+
+        return out;
+    }
+}
