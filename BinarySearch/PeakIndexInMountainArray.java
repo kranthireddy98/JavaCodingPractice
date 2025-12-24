c59712fb806efef2d70feb4729691648aa75def4
kranthi <kranthireddy.mekala@gmail.com>
2025-12-24 21:38:14 +0530

    peakIndexInMountainArray

---
+++ BinarySearch/PeakIndexInMountainArray.java
@@ -0,0 +1,33 @@
+package BinarySearch;
+
+import java.util.List;
+
+public class PeakIndexInMountainArray {
+    public static void main(String[] args) {
+
+        int[]  arr = {0,10,5,2};
+        int index = peakIndexInMountainArray(arr);
+        System.out.println("Peak Index is : " + index);
+    }
+    public static int peakIndexInMountainArray(int[] arr) {
+        int start =0;
+        int end = arr.length-1;
+
+        while(start<=end)
+        {
+            int mid = start + (end - start) /2;
+
+            if(arr[mid] > arr[mid+1])
+            {
+                //in the desc part of array
+                // this may be the ans, but look at left
+                end = mid-1;
+            }else{
+                // in the asc part of array
+                start = mid+1;
+            }
+        }
+
+        return start;
+    }
+}
