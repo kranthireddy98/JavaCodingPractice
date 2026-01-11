3db730ac7cda2861b890a86206a904e3efab02d4
kranthi <kranthireddy.mekala@gmail.com>
2026-01-11 12:02:39 +0530

    Cyclic Sort

---
+++ Sorting/CyclicSort.java
@@ -0,0 +1,36 @@
+package Sorting;
+
+import java.util.Arrays;
+
+/**
+ * When Give range is 1 to N use Cycle sort
+ */
+public class CyclicSort {
+
+    public static void main(String[] args) {
+        int[] arr = {3,5,2,1,4};
+        int[] arr1 = {5,4,3,2,1,0};
+
+        sort(arr1);
+
+        System.out.println(Arrays.toString(arr1));;
+    }
+
+    private static void sort(int[] arr) {
+
+            int i =0;
+            while(i<arr.length )
+            {
+                int correct = arr[i];
+                if(arr[i] != arr[correct]) {
+
+                    int temp = arr[i];
+                    arr[i] = arr[correct];
+                    arr[correct] = temp;
+                }else {
+                    i++;
+            }
+        }
+    }
+
+}
