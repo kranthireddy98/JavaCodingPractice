d791833cf74219f45107c38b28e662303264c5b2
kranthi <kranthireddy.mekala@gmail.com>
2026-01-28 00:18:08 +0530

    recursion

--- Recursion/Factorial.java
+++ Recursion/Factorial.java
@@ -6,13 +6,30 @@ public class Factorial {
     {
         if(n==0) return 1;
 
-        return n * fact(n-1);
+        System.out.println("n " + n);
+        int x = n * fact(n-1);
+        System.out.println("x "+ x);
+        return x;
+    }
 
+    static int fact4(int n){
+        return n * fact3(n - 1);
+    }
 
+    static int fact3(int n){
+        return n * fact2(n - 1);
+    }
+
+    static int fact2(int n){
+        return n * fact1(n - 1);
+    }
+
+    static int fact1(int n){
+        return 1;
     }
 
     public static void main(String[] args) {
-        int res  = fact(4);
+        int res  = fact4(4);
         System.out.println(res);
     }
 }
