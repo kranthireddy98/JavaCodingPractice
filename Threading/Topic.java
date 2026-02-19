b500b3607a248b3d7e62b8fe204a639287fe55b1
kranthi <kranthireddy.mekala@gmail.com>
2026-02-19 23:33:22 +0530

    recursion

--- Threading/Topic.java
+++ Threading/Topic.java
@@ -2,25 +2,32 @@ package Threading;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 public class Topic {
     public static Queue<String> messages = new LinkedList<>();
     public static int capacity =5;
+    public static BlockingQueue<String> messageQue = new ArrayBlockingQueue<>(10);
 
     public synchronized void producer(String message) throws InterruptedException {
-        while (capacity == messages.size()){
+        /*while (capacity == messages.size()){
             wait();
         }
         messages.add(message);
-        notifyAll();
+        notifyAll();*/
+
+        messageQue.add(message);
     }
 
     public synchronized void consumer() throws InterruptedException {
-        while (messages.isEmpty()){
+       /* while (messages.isEmpty()){
             wait();
         }
         String message = messages.poll();
         System.out.println(message);
-        notifyAll();
+        notifyAll();*/
+        String me = messageQue.poll();
+        System.out.println(me);
     }
 }
